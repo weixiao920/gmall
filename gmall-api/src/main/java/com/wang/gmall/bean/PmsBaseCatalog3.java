c34c3d3e97eeb2820bc44105f386593c6edb2799
weixiao920 <920581832@qq.com>
2019-11-14 08:18:25 +0800

    增加三级分类

---
+++ gmall-api/src/main/java/com/wang/gmall/bean/PmsBaseCatalog3.java
@@ -0,0 +1,40 @@
+package com.wang.gmall.bean;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author 微笑
+ * @date 2019/11/13 17:37
+ */
+public class PmsBaseCatalog3 implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+    private String catalog2_id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatalog2_id() {
+        return catalog2_id;
+    }
+
+    public void setCatalog2_id(String catalog2_id) {
+        this.catalog2_id = catalog2_id;
+    }
+}
---
+++ gmall-api/src/main/java/com/wang/gmall/service/CatlogService.java
@@ -0,0 +1,13 @@
+package com.wang.gmall.service;
+
+import com.wang.gmall.bean.PmsBaseCatalog1;
+
+import java.util.List;
+
+/**
+ * @author 微笑
+ * @date 2019/11/13 17:44
+ */
+public interface CatlogService {
+    public List<PmsBaseCatalog1> getCatlog1() ;
+}

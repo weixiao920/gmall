d60a7c602573215accfde4b154d399cb19236930
weixiao920 <920581832@qq.com>
2019-11-15 09:45:30 +0800

    商品spu的属性

---
+++ gmall-api/src/main/java/com/wang/gmall/service/SpuSerivce.java
@@ -0,0 +1,17 @@
+package com.wang.gmall.service;
+
+import com.wang.gmall.bean.PmsBaseSaleAttr;
+import com.wang.gmall.bean.PmsProductInfo;
+import com.wang.gmall.bean.PmsProductSaleAttr;
+
+import java.util.List;
+
+/**
+ * @author 微笑
+ * @date 2019/11/14 20:24
+ */
+public interface SpuSerivce {
+    List<PmsProductInfo> spuList(String catalog3Id);
+
+    List<PmsBaseSaleAttr> getbaseSaleAttrList();
+}
